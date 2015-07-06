266b322188752245dfa8851b5ec47c93d855309a
boschn <boris.schneider@sfk-engineering.de>
2015-07-06 12:34:51 +0200

    developer tested

--- Rulez Workspace/CSVTesting.cs
+++ Rulez Workspace/CSVTesting.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using OnTrack.IO.CSV;
+using System.Diagnostics;
+
 namespace OnTrack.Testing
 {
     [TestClass]
@@ -9,6 +12,24 @@ namespace OnTrack.Testing
         [TestMethod]
         public void MainTesting()
         {
+            /*
+             *  Dim uri As System.Uri = New System.Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase)
+            ''' select the direct path 
+            If String.IsNullOrWhiteSpace(searchpath) Then
+                searchpath = My.Application.Info.DirectoryPath & "\Resources\" & DefaultFolder
+            End If
+             */
+            System.Uri uri = new System.Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+           String path = AppDomain.CurrentDomain.BaseDirectory + "\\Resources\\15_ObjectMessageTypes.csv";
+            System.IO.StreamReader aStream = new System.IO.StreamReader (path);
+            OnTrack.IO.CSV.Reader aReader = new OnTrack.IO.CSV.Reader(aStream, delimiter:";");
+            if (!aReader.Process ()){
+                
+            }
+            else {
+                   Console.Write( aReader.ToString());
+            }
+            Console.Write("done");
         }
     }
 }
