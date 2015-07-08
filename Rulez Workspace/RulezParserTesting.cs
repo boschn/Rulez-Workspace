324313098ca18ed06b7616e6d6d457e632d671b1
boschn <boris.schneider@sfk-engineering.de>
2015-07-08 18:17:45 +0200

    rulez selection grammar angelegt

---
+++ Rulez Workspace/RulezParserTesting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnTrack.Rulez ;
+
+namespace OnTrack.Testing
+{
+    [TestClass]
+    public class RulezParserTesting
+    {
+        private Rulez.Engine _engine = new Rulez.Engine();
+        
+        [TestMethod]
+        public void RulezParserTesting()
+        {
+            try 
+            { 
+                _engine.Generate("selection s1 as deliverables[100];");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
