bb9f1eddc9024dc0b3d15e32ba1ffaad7c3a5bd5
boschn <boris.schneider@sfk-engineering.de>
2015-07-10 13:40:51 +0200

    otDatatype angepasst

--- Rulez Workspace/RulezParserTesting.cs
+++ Rulez Workspace/RulezParserTesting.cs
@@ -9,16 +9,34 @@ namespace OnTrack.Testing
     {
         private Rulez.Engine _engine = new Rulez.Engine();
         
+        // Test-Sources
+        String Test1 = "selection s1 as deliverables[100];";
+        String Test2 = "selection s2 (p1 as number) as deliverables[p1];";
+        String Test3 = "selection s3 (p1 as number) as deliverables[.uid=p1];";
+        String Test4 = "selection s4 as deliverables[.uid=p1 as number];";
         [TestMethod]
-        public void SelectionTesting()
+        public void SelectionTest1()
         {
             try 
             { 
-                _engine.Generate("selection s1 as deliverables[100];");
+                Assert.IsFalse(_engine.Generate(Test1), "Failed: " + Test1 ) ;
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                throw;
+                Assert.Fail(ex.Message);
+            }
+        }
+       
+        [TestMethod]
+        public void SelectionTest2()
+        {
+            try
+            {
+                Assert.IsFalse(_engine.Generate(Test2), "Failed: " + Test2);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
             }
         }
     }
