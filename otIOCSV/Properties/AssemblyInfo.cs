cbb8dd0cc4659d4b5f31d9701ae1c735ee7d0216
boris.schneider <boris.schneider@live.de>
2018-10-22 10:52:55 +0200

    ANTLR update 4.6.6-rc001

--- otIOCSV/Properties/AssemblyInfo.cs
+++ otIOCSV/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1e32ad35-6ffa-4336-825c-d5e038342520")]
